749c439262381a6c1286314a2aabb1371a8a734a
Tim <tim-bellette@users.noreply.github.com>
2022-04-12 08:00:15 +1000

    added initial preference (known bug clearing people when going to an event page without that person).

--- src/events-inject.js
+++ src/events-inject.js
@@ -1,29 +1,214 @@
 $(() => {
+
+    var ENABLE_PREFERENCE = true;
+
+    var attributes = {
+        name: 'name',
+        bookingType: 'bookingtype',
+        gender: 'gender',
+        preference: 'preference'
+    };
+
+    var bookingTypes = {
+        member: 'member',
+        visitor: 'visitor',
+        other: 'other'
+    };
+
+    var genders = {
+        male: 'male',
+        female: 'female'
+    };
+
+    var preferences = {
+        positive: 'positive',
+        ok: 'ok',
+        negative: 'negative'
+    };
+
     $('.teetime-day-table .cell:has(.btn-book-me)').css('top', '-1px').css('border', '0');
     $('.btn-book-me').css('background-color', "#4AA96C");
 
     $('.booking-name').each(function() {
+        var $this = $(this);
         var bookingColor = $(this).css('color');
 
+        //$this.attr('data-name', $this.text().trim());
+        setAttributes($this, {name: $this.text().trim()});
+
         if (bookingColor === 'rgb(0, 0, 0)') {
-            if ($(this).parents('.visitor-cell').length) {
-                $(this).parents('.cell-taken').css('background-color', '#FFCAAD');
+            if ($this.parents('.visitor-cell').length) {
+                // Visitor
+                setAttributes($this, { bookingType: bookingTypes.visitor });
             } else {
                 // Male
-                $(this).parents('.cell-taken').css('background-color', '#A0E7E5');
+                setAttributes($this, { bookingType: bookingTypes.member, gender: genders.male });
             }
 
         } else if (bookingColor === 'rgb(56, 142, 142)') {
             // Female
-            $(this).css('color', '#000000');
-            $(this).parents('.cell-taken').css('background-color', '#FFAEBC');
+            setAttributes($this, { bookingType: bookingTypes.member, gender: genders.female });
         }
         else {
-            // Other
-            $(this).css('color', '#FFF');
-            $(this).parents('.cell-taken').css('background-color', '#9D9D9D');
+            // Other type of book
+            setAttributes($this, { bookingType: bookingTypes.other });
         }
     });
+
+    function setAttributes($element, values) {
+        if (!$element.hasClass('booking-name')) {
+            return; // invalid element
+        }
+
+        for (var attribute in attributes) {
+            if (typeof (values[attribute]) !== 'undefined') {
+                $element.attr(`data-${attribute}`, values[attribute]);
+            }
+        }
+
+        refreshElement($element);
+    }
+
+    function getBooking($element) {
+        var booking = {};
+
+        for (var attribute in attributes) {
+            booking[`${attribute}`] = $element.attr(`data-${attributes[attribute]}`);
+        }
+
+        return booking;
+    }
+
+    function refreshElement($element) {
+        if (!$element.hasClass('booking-name')) {
+            return; // invalid element
+        }
+
+        var $cell = $element.parents('.cell-taken');
+        var booking = getBooking($element);
+        var displayName = booking.name;
+
+        if (booking.bookingType === 'member') {
+            $element.css('color', '#000');
+
+            if (booking.gender === 'male') {
+                displayName += '🧔';
+                $cell.css('background-color', '#A0E7E5');
+            } else if (booking.gender === 'female') {
+                displayName += '👩';
+                $cell.css('background-color', '#FFAEBC')
+            }
+        } else if (booking.bookingType === 'visitor') {
+            $element.css('color', '#000');
+            $cell.css('background-color', '#FFCAAD');
+        } else {
+            $element.css('color', '#FFF');
+            $cell.css('background-color', '#9D9D9D')
+        }
+
+        switch (booking.preference) {
+            case preferences.positive: {
+                displayName = `⭐ ${displayName}`;
+                $cell.css('background-color', '#16C172');
+                break;
+            }
+
+            case preferences.ok: {
+                displayName = `🙂 ${displayName}`;
+                $cell.css('background-color', '#C3E5AE');
+                break;
+            }
+
+            case preferences.negative: {
+                displayName = `⚠ ${displayName}`;
+                $cell.css('background-color', '#F1534E');
+                break;
+            }
+        }
+
+        $element.text(displayName);
+    }
+
+
+    var PreferenceFeature = (() => {
+        let _people = undefined;
+
+        function loadStorage(callback) {
+            chrome.storage.sync.get({
+                people: {
+                    positive: [],
+                    ok: [],
+                    negative: []
+                }
+            }, (items) => {
+                callback(items.people);
+            });
+        }
+
+        function syncStorage() {
+            let people = getPreferencesFromDOM();
+
+            chrome.storage.sync.set({
+                people: people
+            });
+        }
+ 
+
+        function initPreferences() {
+            loadStorage((people) => {
+                initPeople(people);
+                initMenu();
+            });
+        }
+
+        function initPeople(people) {
+            for (var preference in preferences) {
+                if (!people[preference]) 
+                    continue;
+
+                people[preference].forEach((person) => {
+                    $(`.booking-name[data-name="${person}"]`).each(function () {
+                        setAttributes($(this), { preference: preferences[preference] });
+                    });
+                });
+            }
+        }
+
+        function initMenu() {
+            $('.booking-name').each(function () {
+                var cell = $(this).parents('.cell-taken');
+                var name = $(this).attr('data-name');
+                cell.contextMenu({
+                    selector: '.booking-name',
+                    items: {
+                        'name': { name: name, disabled: true },
+                        'positive': { name: `😁 Fun to play with` },
+                        'ok': { name: `🙂 Okay to play with` },
+                        'remove': { name: `😐 Neutral`, callback: function(key, options) {
+                            setAttributes(options.$trigger, { preference: null});
+                            syncStorage();
+                        } },
+                        'negative': { name: `😠 Not fun to play with` }
+                    },
+                    callback: function (key, options) {
+                        setAttributes(options.$trigger, { preference: key });
+                        syncStorage();
+                    }
+                });
+            });
+        }
+
+        return {
+            init: initPreferences
+        };
+
+    })();
+
+    if (ENABLE_PREFERENCE) {
+        PreferenceFeature.init();
+    }
+
+    
 
     if ($('.event-list').length) {
         // Event list page
@@ -40,28 +225,6 @@ $(() => {
                     $available.text(opening);
                     $available.css('color', '#F00');
                     $link.css('color', '#F00');
-
-                    // try {
-                    // let openingDateStr = opening.substr(12, 16);
-                    // let openingDay = openingDateStr.substr(0,2);
-                    // let openingMonth = openingDateStr.substr(3,2);
-                    // let openingYear = openingDateStr.substr(6, 4);
-                    // let openingHour = openingDateStr.substr(11, 2);
-                    // let openingMinute = openingDateStr.substr(14, 2);
-
-                    // let openingDate = new Date(openingYear, openingMonth, openingDay, openingHour, openingMinute);
-
-                    // let distance = dateFns.formatDistance(new Date(), openingDate);
-
-                    // console.log(distance);
-                    // }
-                    // catch (e) {
-                    //     console.log(e);
-                    // }
-
-                    //15-02-2022 17:00
-
-//                    console.log(dateFns.format());
                 }
             });
         }, 1000);
